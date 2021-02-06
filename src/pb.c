20da93304fd219ede47f082f9dd0063781b5625a
mikhail <mike.42.alexeev@gmail.com>
2021-02-07 01:06:10 +0300

    write some protobuf

--- src/pb.c
+++ src/pb.c
@@ -18,9 +18,37 @@ typedef struct pbheader_s {
 
 
 // https://github.com/protobuf-c/protobuf-c/wiki/Examples
+void fill_device_app(DeviceApps *msg) {
+    char *device_id = "e7e1a50c0ec2747ca56cd9e1558c0d7c";
+    char *device_type = "idfa";
+    msg->device->has_id = 1;
+    msg->device->id.data = (uint8_t*)device_id;
+    msg->device->id.len = strlen(device_id);
+    msg->device->has_type = 1;
+    msg->device->type.data = (uint8_t*)device_type;
+    msg->device->type.len = strlen(device_type);
+
+    msg->has_lat = 1;
+    msg->lat = 67.7835424444;
+    msg->has_lon = 1;
+    msg->lon = -22.8044005471;
+
+    msg->n_apps = 3;
+    msg->apps = malloc(sizeof(uint32_t) * msg->n_apps);
+    msg->apps[0] = 42;
+    msg->apps[1] = 43;
+    msg->apps[2] = 44;
+}
+
+void free_device_app(DeviceApps *msg) {
+    free(msg->apps);
+}
+
+
 void example() {
     DeviceApps msg = DEVICE_APPS__INIT;
     DeviceApps__Device device = DEVICE_APPS__DEVICE__INIT;
+
     void *buf;
     unsigned len;
 
@@ -87,14 +115,24 @@ static PyObject* py_deviceapps_xwrite_pb(PyObject* self, PyObject* args) {
 
     while ((item = PyIter_Next(iterator))) {
         pbheader_t header = PBHEADER_INIT;
-        int expected_written_bytes;
-        int written_bytes;
+        void *buf;
+
+        DeviceApps msg = DEVICE_APPS__INIT;
+        DeviceApps__Device device = DEVICE_APPS__DEVICE__INIT;
+        msg.device = &device;
+        fill_device_app(&msg);
+
         /* do something with item */
         /* release reference when done */
-        expected_written_bytes = sizeof(pbheader_t);
-        written_bytes = gzwrite(out, &header, expected_written_bytes);
-        total_written_bytes += written_bytes;
+        header.length = device_apps__get_packed_size(&msg);
+        buf = malloc(header.length);
+        device_apps__pack(&msg, buf);
 
+        total_written_bytes += gzwrite(out, &header, sizeof(pbheader_t));
+        total_written_bytes += gzwrite(out, &buf, header.length);
+
+        free(buf);
+        free_device_app(&msg);
 
         /*PyObject* PyDict_GetItem(PyObject *p, PyObject *key);*/
         Py_DECREF(item);
